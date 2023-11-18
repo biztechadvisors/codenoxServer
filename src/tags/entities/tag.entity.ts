b513d602dad5411273068a25a24443504679378b
Ajay Malviya <biztechadvisors@gmail.com>
2023-11-18 16:58:09 +0530

    Your commit message

--- src/tags/entities/tag.entity.ts
+++ src/tags/entities/tag.entity.ts
@@ -2,7 +2,7 @@ import { Attachment } from 'src/common/entities/attachment.entity';
 import { CoreEntity } from 'src/common/entities/core.entity';
 import { Product } from 'src/products/entities/product.entity';
 import { Type } from 'src/types/entities/type.entity';
-import { Column, Entity, JoinColumn, JoinTable, ManyToMany, OneToMany, OneToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, JoinColumn, JoinTable, ManyToMany, ManyToOne, OneToMany, OneToOne, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class Tag extends CoreEntity {
@@ -21,8 +21,7 @@ export class Tag extends CoreEntity {
   image: Attachment;
   @Column()
   icon: string;
-  @OneToOne(() => Type)
-  @JoinColumn({ name: 'typeId', referencedColumnName: 'id' })
+  @ManyToOne(() => Type, { eager: true })
   type: Type;
   @ManyToMany(() => Product, product => product.tags)
   @JoinTable()
