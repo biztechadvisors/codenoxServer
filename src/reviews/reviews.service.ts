d2180fdbb9d0a7821556178e9e93b5cbbaf78f70
biztechadvisors <advisorsbiztech@gmail.com>
2023-11-21 12:06:23 +0530

    Your commit message here

--- src/reviews/reviews.service.ts
+++ src/reviews/reviews.service.ts
@@ -1,3 +1,110 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { plainToClass } from 'class-transformer';
+import { Any, Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import Fuse from 'fuse.js';
+import { paginate } from 'src/common/pagination/paginate';
+import { CreateReviewDto } from './dto/create-review.dto';
+import { UpdateReviewDto } from './dto/update-review.dto';
+import { GetReviewsDto, ReviewPaginator } from './dto/get-reviews.dto';
+import { Review } from './entities/review.entity';
+
+@Injectable()
+export class ReviewService {
+  constructor(
+    @InjectRepository(Review)
+    private readonly reviewRepository: Repository<Review>,
+  ) {}
+
+  private async getReviewsFromDatabase(): Promise<Review[]> {
+    return await this.reviewRepository.find();
+  }
+  
+
+  private async findReviewInDatabase(id: number): Promise<Review> {
+    const review = await this.reviewRepository.findOne({ where: { id: id } });
+    if (!review) {
+      throw new NotFoundException(`Review with ID ${id} not found`);
+    }
+  
+    return review;
+  }
+  
+  
+
+  private async createReviewInDatabase(createReviewDto: CreateReviewDto): Promise<Review> {
+    const review = plainToClass(Review, createReviewDto);
+    return await this.reviewRepository.save(review);
+  }
+
+  private async updateReviewInDatabase(id: number, updateReviewDto: UpdateReviewDto): Promise<Review> {
+    const review = await this.findReviewInDatabase(id); // Ensure the review exists
+  
+    // Update the review entity with the new data
+    Object.assign(review, updateReviewDto);
+  
+    // Save the updated review to the database
+    return await this.reviewRepository.save(review);
+  }
+  
+  
+
+  private async deleteReviewInDatabase(id: number): Promise<void> {
+    const review = await this.findReviewInDatabase(id);
+    await this.reviewRepository.remove(review);
+  }
+
+  async findAllReviews({ limit, page, search, product_id }: GetReviewsDto): Promise<ReviewPaginator> {
+    let reviews = await this.getReviewsFromDatabase();
+
+    if (search) {
+      const parseSearchParams = search.split(';');
+      for (const searchParam of parseSearchParams) {
+        const [key, value] = searchParam.split(':');
+        const options = {
+          keys: [key],
+          threshold: 0.3,
+        };
+        const fuse = new Fuse(reviews, options);
+        reviews = fuse.search(value)?.map(({ item }) => item) || [];
+      }
+    }
+
+    if (product_id) {
+      reviews = reviews.filter((p) => p.product_id === Number(product_id));
+    }
+
+    const startIndex = (page - 1) * limit;
+    const endIndex = page * limit;
+    const results = reviews.slice(startIndex, endIndex);
+    const url = `/reviews?search=${search}&limit=${limit}`;
+
+    return {
+      data: results,
+      ...paginate(reviews.length, page, limit, results.length, url),
+    };
+  }
+
+  async findReview(id: number): Promise<Review> {
+    return await this.findReviewInDatabase(id);
+  }
+
+  async create(createReviewDto: CreateReviewDto): Promise<Review> {
+    return await this.createReviewInDatabase(createReviewDto);
+  }
+
+  async update(id: number, updateReviewDto: UpdateReviewDto): Promise<Review> {
+    return await this.updateReviewInDatabase(id, updateReviewDto);
+  }
+
+  async delete(id: number): Promise<void> {
+    return await this.deleteReviewInDatabase(id);
+  }
+  
+}
+
+
+
 // import { Injectable } from '@nestjs/common';
 // import { plainToClass } from 'class-transformer';
 // import Fuse from 'fuse.js';
@@ -62,101 +169,3 @@
 //     return this.reviews[0];
 //   }
 // }
-
-
-
-import { Injectable, NotFoundException } from '@nestjs/common';
-import { plainToClass } from 'class-transformer';
-import { Repository } from 'typeorm';
-import { InjectRepository } from '@nestjs/typeorm';
-import Fuse from 'fuse.js';
-import { paginate } from 'src/common/pagination/paginate';
-import { CreateReviewDto } from './dto/create-review.dto';
-import { UpdateReviewDto } from './dto/update-review.dto';
-import { GetReviewsDto, ReviewPaginator } from './dto/get-reviews.dto';
-import { Review } from './entities/review.entity';
-
-@Injectable()
-export class ReviewService {
-  constructor(
-    @InjectRepository(Review)
-    private readonly reviewRepository: Repository<Review>,
-  ) {}
-
-  private async getReviewsFromDatabase(): Promise<Review[]> {
-    return await this.reviewRepository.find();
-  }
-  
-
-  private async findReviewInDatabase(id: any): Promise<Review> {
-    const review = await this.reviewRepository.findOne(id);
-    if (!review) {
-      throw new NotFoundException(`Review with ID ${id} not found`);
-    }
-    return review;
-  }
-
-  private async createReviewInDatabase(createReviewDto: CreateReviewDto): Promise<Review> {
-    const review = plainToClass(Review, createReviewDto);
-    return await this.reviewRepository.save(review);
-  }
-
-  private async updateReviewInDatabase(id: number, updateReviewDto: UpdateReviewDto): Promise<Review> {
-    await this.findReviewInDatabase(id); // Ensure the review exists
-
-    await this.reviewRepository.update(id, UpdateReviewDto);
-    return await this.findReviewInDatabase(id);
-  }
-
-  private async deleteReviewInDatabase(id: number): Promise<void> {
-    const review = await this.findReviewInDatabase(id);
-    await this.reviewRepository.remove(review);
-  }
-
-  async findAllReviews({ limit, page, search, product_id }: GetReviewsDto): Promise<ReviewPaginator> {
-    let reviews = await this.getReviewsFromDatabase();
-
-    if (search) {
-      const parseSearchParams = search.split(';');
-      for (const searchParam of parseSearchParams) {
-        const [key, value] = searchParam.split(':');
-        const options = {
-          keys: [key],
-          threshold: 0.3,
-        };
-        const fuse = new Fuse(reviews, options);
-        reviews = fuse.search(value)?.map(({ item }) => item) || [];
-      }
-    }
-
-    if (product_id) {
-      reviews = reviews.filter((p) => p.product_id === Number(product_id));
-    }
-
-    const startIndex = (page - 1) * limit;
-    const endIndex = page * limit;
-    const results = reviews.slice(startIndex, endIndex);
-    const url = `/reviews?search=${search}&limit=${limit}`;
-
-    return {
-      data: results,
-      ...paginate(reviews.length, page, limit, results.length, url),
-    };
-  }
-
-  async findReview(id: number): Promise<Review> {
-    return await this.findReviewInDatabase(id);
-  }
-
-  async create(createReviewDto: CreateReviewDto): Promise<Review> {
-    return await this.createReviewInDatabase(createReviewDto);
-  }
-
-  async update(id: number, updateReviewDto: UpdateReviewDto): Promise<Review> {
-    return await this.updateReviewInDatabase(id, updateReviewDto);
-  }
-
-  async delete(id: number): Promise<void> {
-    return await this.deleteReviewInDatabase(id);
-  }
-}
