45ee58b0dd0488ed4e3c34507014d01b072d6769
ayushjik <advisorsbiztech@gmail.com>
2024-01-19 13:50:32 +0530

    Ayush_19/01

--- src/taxes/taxes.controller.ts
+++ src/taxes/taxes.controller.ts
@@ -10,17 +10,24 @@ import {
   Query,
 } from '@nestjs/common';
 import { TaxesService } from './taxes.service';
-import { CreateTaxDto } from './dto/create-tax.dto';
+import { CreateTaxDto, ValidateGstDto } from './dto/create-tax.dto';
 import { UpdateTaxDto } from './dto/update-tax.dto';
 import { GetTaxesDto } from './dto/get-taxes.dto';
 
 @Controller('taxes')
 export class TaxesController {
-  constructor(private readonly taxesService: TaxesService) { }
+  constructor(private readonly taxesService: TaxesService) {}
 
   @Post()
   create(@Body() createTaxDto: CreateTaxDto) {
     return this.taxesService.create(createTaxDto);
+  }
+
+
+  @Post('/validate-gst')
+  createValidateGST(@Body() validateGstDto:ValidateGstDto) {
+    console.log(validateGstDto);
+    return this.taxesService.validateGST(validateGstDto.gstNumber);
   }
 
   @Get()
@@ -32,11 +39,16 @@ export class TaxesController {
   findOne(@Param('id') id: string) {
     return this.taxesService.findOne(+id);
   }
+  @Get(':id/:proId')
+  findOnePro(@Param('id') id: string, @Param('proId') proId: string) {
+    return this.taxesService.findOnePro(+id, +proId);
+  }
 
   @Put(':id')
   update(@Param('id') id: string, @Body() updateTaxDto: UpdateTaxDto) {
     return this.taxesService.update(+id, updateTaxDto);
   }
+
 
   @Delete(':id')
   remove(@Param('id') id: string) {
--- src/taxes/taxes.module.ts
+++ src/taxes/taxes.module.ts
@@ -4,9 +4,11 @@ import { TaxesService } from './taxes.service';
 import { TaxesController } from './taxes.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Tax } from './entities/tax.entity';
+import { Product } from 'src/products/entities/product.entity';
+import { Category } from 'src/categories/entities/category.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Tax])],
+  imports: [TypeOrmModule.forFeature([Tax,Product,Category])],
   controllers: [TaxesController],
   providers: [TaxesService],
 })
