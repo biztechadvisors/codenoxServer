b8b3f91208ef185f9cdf6a7e3cda9bbd99355dfb
Ajay Malviya <biztechadvisors@gmail.com>
2024-02-27 13:00:04 +0530

    commit push

--- src/analytics/analytics.service.ts
+++ src/analytics/analytics.service.ts
@@ -125,7 +125,7 @@ export class AnalyticsService {
   private async calculateTotalShops(userId: number, permissionName: string, state: string): Promise<number> {
     try {
 
-      if (permissionName !== 'Admin') {
+      if (permissionName !== 'super_admin' && permissionName !== 'Admin') {
         return 0;
       }
 
