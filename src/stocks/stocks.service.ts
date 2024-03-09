0dfd60b87d8e340d81118a9ac2e0d7770ee65d89
Ajay Malviya <biztechadvisors@gmail.com>
2024-03-09 20:17:19 +0530

    commit push

--- src/stocks/stocks.service.ts
+++ src/stocks/stocks.service.ts
@@ -18,6 +18,9 @@ import { MailService } from 'src/mail/mail.service';
 import { Coupon } from 'src/coupons/entities/coupon.entity';
 import { UserAddress } from 'src/addresses/entities/address.entity';
 import { Shop } from 'src/shops/entities/shop.entity';
+import { GetOrdersDto, OrderPaginator } from 'src/orders/dto/get-orders.dto';
+import { Permission } from 'src/permission/entities/permission.entity';
+import { paginate } from 'src/common/pagination/paginate';
 
 @Injectable()
 export class StocksService {
@@ -45,6 +48,8 @@ export class StocksService {
         private readonly userAddressRepository: Repository<UserAddress>,
         @InjectRepository(Shop)
         private readonly shopRepository: Repository<Shop>,
+        @InjectRepository(Permission)
+        private readonly permissionRepository: Repository<Permission>,
     ) { }
 
     async create(createStocksDto: CreateStocksDto): Promise<Stocks[]> {
@@ -292,6 +297,202 @@ export class StocksService {
         } catch (error) {
             console.error('Error creating order:', error);
             throw error;
+        }
+    }
+
+    async getOrders({
+        limit,
+        page,
+        customer_id,
+        tracking_number,
+        search,
+        shop_id,
+    }: GetOrdersDto): Promise<OrderPaginator> {
+        try {
+
+            console.log("Query********", limit,
+                page,
+                customer_id,
+                tracking_number,
+                search,
+                shop_id,)
+
+            const usr = await this.userRepository.findOne({ where: { id: customer_id }, relations: ['type'] });
+
+            console.log("user*****", usr)
+            if (!usr) {
+                throw new Error('User not found');
+            }
+
+            // Fetch permissions for the user
+            const permsn = await this.permissionRepository.findOneBy(usr.type);
+
+            let query = this.StocksSellOrdRepository.createQueryBuilder('StocksSellOrd');
+            query = query.leftJoinAndSelect('StocksSellOrd.status', 'status');
+            query = query.leftJoinAndSelect('StocksSellOrd.dealer', 'dealer');
+            query = query.leftJoinAndSelect('StocksSellOrd.billing_address', 'billing_address');
+            query = query.leftJoinAndSelect('StocksSellOrd.shipping_address', 'shipping_address');
+            query = query.leftJoinAndSelect('StocksSellOrd.customer', 'customer');
+            query = query.leftJoinAndSelect('StocksSellOrd.products', 'products')
+                .leftJoinAndSelect('products.pivot', 'pivot')
+                .leftJoinAndSelect('products.taxes', 'taxes')
+                .leftJoinAndSelect('products.variation_options', 'variation_options');
+            // query = query.leftJoinAndSelect('StocksSellOrd.payment_intent', 'payment_intent')
+            //     .leftJoinAndSelect('payment_intent.payment_intent_info', 'payment_intent_info');
+            query = query.leftJoinAndSelect('StocksSellOrd.shop_id', 'shop');
+            query = query.leftJoinAndSelect('StocksSellOrd.coupon', 'coupon');
+
+            if (!(permsn && (permsn.type_name === 'Admin' || permsn.type_name === 'super_admin'))) {
+                // If the user has other permissions, filter orders by customer_id
+                const usrByIdUsers = await this.userRepository.find({
+                    where: { UsrBy: { id: usr.id } }, relations: ['type']
+                });
+
+                const userIds = [usr.id, ...usrByIdUsers.map(user => user.id)];
+                query = query.andWhere('order.customer.id IN (:...userIds)', { userIds });
+            }
+
+            // Handle additional filtering conditions
+            if (shop_id && shop_id !== 'undefined') {
+                query = query.andWhere('products.shop_id = :shopId', { shopId: Number(shop_id) });
+            }
+
+            if (search) {
+                query = query.andWhere('(status.name ILIKE :searchValue OR order.fieldName ILIKE :searchValue)', {
+                    searchValue: `%${search}%`,
+                });
+            }
+
+            if (tracking_number) {
+                query = query.andWhere('order.tracking_number = :trackingNumber', { trackingNumber: tracking_number });
+            }
+
+            // Handle pagination
+            if (!page) page = 1;
+            if (!limit) limit = 15;
+            const startIndex = (page - 1) * limit;
+
+            const [data, totalCount] = await query
+                .skip(startIndex)
+                .take(limit)
+                .getManyAndCount();
+
+            console.log("data*****", data)
+            const results = await Promise.all(
+                data.map(async (order) => {
+                    const products = await Promise.all(order.products.map(async (product) => {
+                        // Fetch pivot data for the current product based on the Ord_Id
+                        const pivot = await this.orderProductPivotRepository.findOne({
+                            where: {
+                                product: { id: product.id },
+                                Ord_Id: order.id, // Use Ord_Id instead of order.id
+                            },
+                        });
+
+                        // If pivot is undefined, return null or handle appropriately
+                        if (!pivot) {
+                            return null; // Or handle this case appropriately
+                        }
+
+                        return {
+                            id: product.id,
+                            name: product.name,
+                            slug: product.slug,
+                            description: product.description,
+                            type_id: product.type_id,
+                            price: product.price,
+                            shop_id: product.shop_id,
+                            sale_price: product.sale_price,
+                            language: product.language,
+                            min_price: product.min_price,
+                            max_price: product.max_price,
+                            sku: product.sku,
+                            quantity: product.quantity,
+                            in_stock: product.in_stock,
+                            is_taxable: product.is_taxable,
+                            shipping_class_id: null,
+                            status: product.status,
+                            product_type: product.product_type,
+                            unit: product.unit,
+                            height: product.height ? product.height : null,
+                            width: product.width ? product.width : null,
+                            length: product.length ? product.length : null,
+                            image: product.image,
+                            video: null,
+                            gallery: product.gallery,
+                            deleted_at: null,
+                            created_at: product.created_at,
+                            updated_at: product.updated_at,
+                            author_id: null,
+                            manufacturer_id: null,
+                            is_digital: 0,
+                            is_external: 0,
+                            external_product_url: null,
+                            external_product_button_text: null,
+                            ratings: product.ratings,
+                            total_reviews: product.my_review,
+                            rating_count: product.ratings,
+                            my_review: product.my_review,
+                            in_wishlist: product.in_wishlist,
+                            blocked_dates: [],
+                            translated_languages: product.translated_languages,
+                            taxes: product.taxes,
+                            pivot: pivot, // Use the found pivot
+                            variation_options: product.variation_options,
+                        };
+                    }));
+
+                    return {
+                        id: order.id,
+                        tracking_number: order.tracking_number,
+                        customer_id: order.customer_id,
+                        customer_contact: order.customer_contact,
+                        amount: order.amount,
+                        sales_tax: order.sales_tax,
+                        paid_total: order.paid_total,
+                        total: order.total,
+                        cancelled_amount: order?.cancelled_amount,
+                        language: order?.language,
+                        coupon_id: order.coupon,
+                        saleBy: order?.saleBy,
+                        discount: order?.discount,
+                        payment_gateway: order.payment_gateway,
+                        shipping_address: order.shipping_address,
+                        billing_address: order.billing_address,
+                        logistics_provider: order.logistics_provider,
+                        delivery_fee: order.delivery_fee,
+                        delivery_time: order.delivery_time,
+                        order_status: order.order_status,
+                        payment_status: order.payment_status,
+                        created_at: order.created_at,
+                        payment_intent: order.payment_intent,
+                        customer: {
+                            id: order.customer.id,
+                            name: order.customer.name,
+                            email: order.customer.email,
+                            email_verified_at: order.customer.email_verified_at,
+                            created_at: order.customer.created_at,
+                            updated_at: order.customer.updated_at,
+                            is_active: order.customer.is_active,
+                            shop_id: null
+                        },
+                        dealer: order.dealer ? order.dealer : null,
+                        products: products.filter(product => product !== null), // Exclude products for which pivot data could not be fetched
+                        // children: order.children,
+                        wallet_point: order?.wallet_point
+                    };
+                })
+            );
+
+            console.log("Orders******", results)
+            const url = `/orders?search=${search}&limit=${limit}`;
+            return {
+                data: results,
+                ...paginate(totalCount, page, limit, results.length, url),
+            };
+        } catch (error) {
+            console.error('Error in getOrders:', error);
+            throw error; // rethrow the error for further analysis
         }
     }
 
