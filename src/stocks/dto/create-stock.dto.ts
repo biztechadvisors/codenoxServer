edafa6397b60d81628d8380a8836f0f5afde2d96
Ajay Malviya <biztechadvisors@gmail.com>
2024-05-04 19:51:56 +0530

    commit for change

--- src/stocks/dto/create-stock.dto.ts
+++ src/stocks/dto/create-stock.dto.ts
@@ -2,7 +2,7 @@ import { Product } from 'src/products/entities/product.entity';
 import { User } from 'src/users/entities/user.entity';
 
 export class CreateStocksDto {
-    products: Product[];
+    products: any[];
     ordPendQuant: number;
     dispatchedQuantity: number;
     status: boolean;
