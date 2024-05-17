04578369e5eaef61b493753ecf5384bcdb877502
Ajay Malviya <biztechadvisors@gmail.com>
2024-05-17 15:18:14 +0530

    commit for change

--- src/uploads/uploads.service.ts
+++ src/uploads/uploads.service.ts
@@ -19,24 +19,19 @@ export class UploadsService {
     secretAccessKey: this.configService.get('AWS_SECRET_KEY'),
   });
 
-  async uploadFiles(files: Express.Multer.File | Express.Multer.File[]): Promise<{ data: { upload: AttachmentDTO[] } }> {
+  async uploadFiles(files: Express.Multer.File | Express.Multer.File[]) {
     const filesArray = Array.isArray(files) ? files : [files];
     const uploadPromises = filesArray.map(file => this.uploadToS3(file.buffer, file.originalname, file.mimetype));
     const results = await Promise.all(uploadPromises);
 
     // Construct the response object
-    const uploadData = {
-      data: {
-        upload: results.map(result => ({
-          original: result.original,
-          thumbnail: result.thumbnail,
-          id: result.id,
-          __typename: 'Attachment'
-        }))
-      }
-    };
+    const attachments = results.map(result => ({
+      original: result.original,
+      thumbnail: result.thumbnail,
+      id: result.id,
+    }))
 
-    return uploadData;
+    return attachments;
   }
 
 
@@ -51,7 +46,7 @@ export class UploadsService {
     try {
       const uploadResult = await this.s3.upload(params).promise();
       const attachmentDTO = new AttachmentDTO();
-      attachmentDTO.original = originalname;
+      attachmentDTO.original = uploadResult.Location;
       attachmentDTO.thumbnail = uploadResult.Location;
 
       return await this.attachmentRepository.save(attachmentDTO);
