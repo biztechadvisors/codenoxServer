f1ce90d35018a5b88c5e68f84f4299d20d6a3c59
Ajay Malviya <biztechadvisors@gmail.com>
2024-07-27 15:58:09 +0530

    commit server-8

--- src/stocks/dto/create-stock.dto.ts
+++ src/stocks/dto/create-stock.dto.ts
@@ -1,4 +1,7 @@
 
+import { CardInput, ConnectProductOrderPivot, UserAddressInput } from 'src/orders/dto/create-order.dto';
+import { PaymentGatewayType } from 'src/orders/entities/order.entity';
+import { PaymentIntent } from 'src/payment-intent/entries/payment-intent.entity';
 import { Product } from 'src/products/entities/product.entity';
 import { User } from 'src/users/entities/user.entity';
 
@@ -34,4 +37,27 @@ export class UpdateInvStkQuantityDto {
     order_id: number;
     product_id: number;
     updateReceivedQuantity: number;
+}
+export class CreatestockOrderDto {
+    saleBy?: UserAddressInput
+    soldBy?: User
+    coupon_id?: number
+    status: string
+    customer_contact: string
+    products: ConnectProductOrderPivot[]
+    amount: number
+    sales_tax: number
+    total?: number
+    paid_total?: number
+    payment_id?: string
+    payment_gateway?: PaymentGatewayType
+    discount?: number
+    delivery_fee?: number
+    delivery_time: string
+    card?: CardInput
+    billing_address?: UserAddressInput
+    shipping_address?: UserAddressInput
+    payment_intent: PaymentIntent
+    language?: string
+    dealerId?: User
 }
